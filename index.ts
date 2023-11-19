f93c1fe6646a76918ec2de3bbad13f500caa2c92
UmairKhan2324 <asadsher2324@gmail.com>
2023-11-19 11:01:58 +0500

    modified index.ts

--- index.ts
+++ index.ts
@@ -5,5 +5,16 @@ const answers = await inquirer.prompt([
     type: "input",
     name: "UserID",
     message: "kindly enter your ID",
-  }  
+  },
+  {
+    type: "number",
+    name: "Password",
+    message: "kindly enter your password",
+  },
+  {
+    type: "list",
+    name: "transcations",
+    choices: ["fast cash","withdraw"],
+    message: "kindly select your transaction",
+  }
 ])
